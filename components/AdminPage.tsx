c2193346e16d684840e4eee69a88a192581531c5
hashrock <hashrock@users.noreply.github.com>
2023-07-03 03:52:04 +0900

    add login wall

--- components/AdminPage.tsx
+++ components/AdminPage.tsx
@@ -14,9 +14,22 @@ export function AdminPage(props: PageProps) {
         (props.class ? " " + props.class : "")}
     >
       <LoginNav user={props.user} />
-      <div>
-        {props.children}
-      </div>
+      {props.user
+        ? (
+          <div>
+            {props.children}
+          </div>
+        )
+        : (
+          <div class="flex justify-center mt-16">
+            <a
+              class="inline-block px-3 py-2 bg-blue-800 text-white hover:bg-blue-700 rounded"
+              href="/auth/signin"
+            >
+              Log in
+            </a>
+          </div>
+        )}
     </div>
   );
 }
